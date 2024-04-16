39f2c2a5ae1cbf0b82aeea5bcbdeb48b11545c03
Kira Sokolova <Kyra256@proton.me>
2024-04-16 17:02:37 +0300

    feat(di): create AppModule

---
+++ app/src/main/java/io/github/kirasok/photogallerycompose/di/AppModule.kt
@@ -0,0 +1,11 @@
+package io.github.kirasok.photogallerycompose.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
