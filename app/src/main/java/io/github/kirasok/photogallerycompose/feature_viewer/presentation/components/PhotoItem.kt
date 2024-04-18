cc86f08143d4282b6e1bb2b46273dfd3722498c2
Kira Sokolova <Kyra256@proton.me>
2024-04-18 20:21:51 +0300

    feat(presentation): crop image

--- app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/presentation/components/PhotoItem.kt
+++ app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/presentation/components/PhotoItem.kt
@@ -1,10 +1,21 @@
 package io.github.kirasok.photogallerycompose.feature_viewer.presentation.components
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import io.github.kirasok.photogallerycompose.feature_viewer.domain.model.Photo
 
 @Composable
 fun PhotoItem(photo: Photo) {
-  AsyncImage(model = photo.url, contentDescription = photo.title)
+  AsyncImage(
+    model = photo.url,
+    contentDescription = photo.title,
+    modifier = Modifier.clip(RectangleShape).height(120.dp),
+    contentScale = ContentScale.Crop
+  )
 }
