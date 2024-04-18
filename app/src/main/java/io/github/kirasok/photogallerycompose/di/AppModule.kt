6950f9f677b185822df6b810a92e396af55a16a3
Kira Sokolova <Kyra256@proton.me>
2024-04-18 15:59:53 +0300

    feat(data): FlickrAPI

--- app/src/main/java/io/github/kirasok/photogallerycompose/di/AppModule.kt
+++ app/src/main/java/io/github/kirasok/photogallerycompose/di/AppModule.kt
@@ -1,11 +1,31 @@
 package io.github.kirasok.photogallerycompose.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.kirasok.photogallerycompose.BuildConfig
+import io.github.kirasok.photogallerycompose.feature_viewer.data.api.PhotoInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+  @Singleton
+  @Provides
+  fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+    .baseUrl("https://api.flickr.com/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(okHttpClient)
+    .build()
+
+  @Singleton
+  @Provides
+  fun provideOkHttpClint() = OkHttpClient.Builder().addInterceptor(PhotoInterceptor(BuildConfig.API_KEY))
+    .build() //TODO: store API_KEY in shared preferences and ask user on login
 
 }
--- app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/data/api/FlickrApi.kt
+++ app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/data/api/FlickrApi.kt
@@ -1,4 +1,22 @@
 package io.github.kirasok.photogallerycompose.feature_viewer.data.api
 
+import io.github.kirasok.photogallerycompose.feature_viewer.domain.model.Photo
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface FlickrApi {
+
+  @GET("services/rest?method=flickr.interestingness.getList")
+  suspend fun getPopularPhotos(
+    @Query("per_page") perPage: Int,
+    @Query("page") page: Int,
+  ): Response<List<Photo>>
+
+  @GET("services/rest?method=flickr.photos.search&sort=relevance")
+  suspend fun searchPhotos(
+    @Query("per_page") perPage: Int,
+    @Query("text") query: String,
+  ): Response<List<Photo>>
+
 }
