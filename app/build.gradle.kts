c062fb94d828b58e48607768535fd5b705e59ed5
Kira Sokolova <Kyra256@proton.me>
2024-04-18 15:20:01 +0300

    feat(gradle): add Gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material3)
   implementation(libs.hilt.android)
+  implementation(libs.converter.gson)
   ksp(libs.hilt.compiler)
   implementation(libs.androidx.lifecycle.viewmodel.compose)
   implementation(libs.androidx.navigation.compose)
