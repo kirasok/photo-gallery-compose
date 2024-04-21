9abcf90f3b72871b9d51e5c46f3d004c71534e12
Kira Sokolova <Kyra256@proton.me>
2024-04-21 13:43:37 +0300

    feat(data): log requests and responses

--- app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/data/api/PhotoInterceptor.kt
+++ app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/data/api/PhotoInterceptor.kt
@@ -1,5 +1,6 @@
 package io.github.kirasok.photogallerycompose.feature_viewer.data.api
 
+import android.util.Log
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -9,6 +10,8 @@ class PhotoInterceptor(private val apiKey: String) : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     val originalRequest = chain.request()
 
+    Log.d(null, "Got original request: $originalRequest")
+
     val newUrl: HttpUrl = originalRequest.url.newBuilder().apply {
       addQueryParameter("api_key", apiKey)
       addQueryParameter("format", "json")
@@ -17,6 +20,8 @@ class PhotoInterceptor(private val apiKey: String) : Interceptor {
     }.build()
 
     val newRequest: Request = originalRequest.newBuilder().url(newUrl).build()
+
+    Log.d(null, "Made new request: $newRequest")
 
     return chain.proceed(newRequest)
   }
