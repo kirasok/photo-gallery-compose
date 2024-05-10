a0f971d1bea8e364152787f2d2f0de0c60456a56
Kira Sokolova <Kyra256@proton.me>
2024-05-11 01:46:51 +0300

    refactor(presentation): create separate PhotoGrid

--- app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/presentation/components/PhotoItem.kt
+++ app/src/main/java/io/github/kirasok/photogallerycompose/feature_viewer/presentation/components/PhotoItem.kt
@@ -6,16 +6,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import io.github.kirasok.photogallerycompose.feature_viewer.domain.model.Photo
 
 @Composable
-fun PhotoItem(photo: Photo) {
+fun PhotoItem(photo: Photo, height: Dp = 120.dp) {
   AsyncImage(
     model = photo.url,
     contentDescription = photo.title,
-    modifier = Modifier.clip(RectangleShape).height(120.dp),
+    modifier = Modifier
+      .clip(RectangleShape)
+      .height(height),
     contentScale = ContentScale.Crop
   )
 }
